cdf0481c62b1554f8d3182bc0a6331e5f494125c
Aniruddha Bolakhe <68197577+AniruddhaBolakhe@users.noreply.github.com>
2025-02-24 16:57:35 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -70,7 +70,7 @@ def fetch_gemini_insights(tumor_type):
 st.title(" Brain Tumor Detection ")
 st.markdown("""
 system trained on neural networks to assist in identifying brain tumors through MRI scans. 
-Supports the  tumor types:
+=tumor types(Supported):
 - **Glioma Tumor**
 - **Meningioma Tumor**
 - **Pituitary Tumor**
